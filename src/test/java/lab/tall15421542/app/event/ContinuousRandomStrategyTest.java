9601abae1d31e9fa6a5d154b49ff56a6dcb586f3
tall15421542-lab <tall15421542@gmail.com>
2025-01-24 15:56:41 +0800

    add continous random test

--- src/test/java/lab/tall15421542/app/event/ContinuousRandomStrategyTest.java
+++ src/test/java/lab/tall15421542/app/event/ContinuousRandomStrategyTest.java
@@ -4,6 +4,7 @@ import lab.tall15421542.app.avro.event.AreaStatus;
 import lab.tall15421542.app.avro.event.ReserveSeat;
 import lab.tall15421542.app.avro.event.SeatStatus;
 import lab.tall15421542.app.avro.reservation.*;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,66 +12,80 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SelfPickStrategyTest {
+class ContinuousRandomStrategyTest {
     @Test
     void successReservation(){
         List<List<SeatStatus>> seats = new ArrayList<>();
-        for(int i = 0 ; i < 2 ; ++i){
+        for(int i = 0 ; i < 3 ; ++i){
             seats.add(new ArrayList<>());
-            for(int j = 0 ; j < 2 ; ++j){
+            for(int j = 0 ; j < 3 ; ++j){
                 seats.get(i).add(new SeatStatus(i, j, true));
             }
         }
         AreaStatus areaStatus = new AreaStatus(
-                "event", "A", 100, 2, 2, 4, seats
+                "event", "A", 100, 3, 3, 9, seats
         );
 
-        List<Seat> requestedSeats = new ArrayList<>();
-        requestedSeats.add(new Seat(0, 0));
-        requestedSeats.add(new Seat(0, 1));
         ReserveSeat req = new ReserveSeat(
-                "reservationId", "event", "A", 2, 2, ReservationTypeEnum.SELF_PICK, requestedSeats
+                "reservationId", "event", "A", 2, 2, ReservationTypeEnum.RANDOM, new ArrayList<>()
         );
 
-        Service.ReservationStrategy strategy = new SelfPickStrategy();
+        Service.ReservationStrategy strategy = new ContinuousRandomStrategy();
 
         ReservationResult expectedResult = new ReservationResult();
         expectedResult.setReservationId("reservationId");
         expectedResult.setResult(ReservationResultEnum.SUCCESS);
-        expectedResult.setSeats(requestedSeats);
+        List<Seat> expectedSeats = new ArrayList<>();
+        expectedSeats.add(new Seat(0, 0));
+        expectedSeats.add(new Seat(0, 1));
+        expectedResult.setSeats(expectedSeats);
 
+        assertEquals(expectedResult, strategy.reserve(areaStatus, req));
+
+        areaStatus.getSeats().get(0).get(0).setIsAvailable(false);
+        areaStatus.getSeats().get(0).get(1).setIsAvailable(false);
+        areaStatus.getSeats().get(1).get(1).setIsAvailable(false);
+        areaStatus.getSeats().get(2).get(0).setIsAvailable(false);
+        areaStatus.setAvailableSeats(5);
+        expectedSeats.clear();
+        expectedSeats.add(new Seat(2,1));
+        expectedSeats.add(new Seat(2,2));
+        expectedResult.setSeats(expectedSeats);
         assertEquals(expectedResult, strategy.reserve(areaStatus, req));
     }
 
     @Test
     void notAvailableReservation(){
         List<List<SeatStatus>> seats = new ArrayList<>();
-        for(int i = 0 ; i < 2 ; ++i){
+        for(int i = 0 ; i < 3 ; ++i){
             seats.add(new ArrayList<>());
-            for(int j = 0 ; j < 2 ; ++j){
-                seats.get(i).add(new SeatStatus(i, j, false));
+            for(int j = 0 ; j < 3 ; ++j){
+                seats.get(i).add(new SeatStatus(i, j, true));
             }
         }
+        seats.get(0).get(1).setIsAvailable(false);
+        seats.get(1).get(1).setIsAvailable(false);
+        seats.get(2).get(1).setIsAvailable(false);
         AreaStatus areaStatus = new AreaStatus(
-                "event", "A", 100, 2, 2, 4, seats
+                "event", "A", 100, 3, 3, 6, seats
         );
 
-        List<Seat> requestedSeats = new ArrayList<>();
-        requestedSeats.add(new Seat(0, 0));
-        requestedSeats.add(new Seat(0, 1));
         ReserveSeat req = new ReserveSeat(
-                "reservationId", "event", "A", 2, 2, ReservationTypeEnum.SELF_PICK, requestedSeats
+                "reservationId", "event", "A", 2, 2, ReservationTypeEnum.RANDOM, new ArrayList<>()
         );
 
-        Service.ReservationStrategy strategy = new SelfPickStrategy();
+        Service.ReservationStrategy strategy = new ContinuousRandomStrategy();
 
         ReservationResult expectedResult = new ReservationResult();
         expectedResult.setReservationId("reservationId");
         expectedResult.setResult(ReservationResultEnum.FAILED);
         expectedResult.setErrorCode(ReservationErrorCodeEnum.NOT_AVAILABLE);
-        expectedResult.setErrorMessage("event#A (0, 0) is unavailable.");
+        expectedResult.setErrorMessage("no continuous 2 seats at area A in event event");
 
         assertEquals(expectedResult, strategy.reserve(areaStatus, req));
+
+        req.setNumOfSeats(4);
+        expectedResult.setErrorMessage("no continuous 4 seats at area A in event event");
     }
 
     @Test
@@ -86,38 +101,21 @@ class SelfPickStrategyTest {
                 "event", "A", 100, 2, 2, 4, seats
         );
 
-        List<Seat> requestedSeats = new ArrayList<>();
-        requestedSeats.add(new Seat(0, 1));
-        requestedSeats.add(new Seat(0, 2));
         ReserveSeat req = new ReserveSeat(
-                "reservationId", "event", "A", 2, 2, ReservationTypeEnum.SELF_PICK, requestedSeats
+                "reservationId", "event", "A", -1, -1, ReservationTypeEnum.RANDOM, new ArrayList<>()
         );
 
-        Service.ReservationStrategy strategy = new SelfPickStrategy();
+        Service.ReservationStrategy strategy = new ContinuousRandomStrategy();
 
         ReservationResult expectedResult = new ReservationResult();
         expectedResult.setReservationId("reservationId");
         expectedResult.setResult(ReservationResultEnum.FAILED);
-        expectedResult.setErrorCode(ReservationErrorCodeEnum.INVALID_SEAT);
-        expectedResult.setErrorMessage("event#A (0, 2) is not a valid seat.");
+        expectedResult.setErrorCode(ReservationErrorCodeEnum.INVALID_ARGUMENT);
+        expectedResult.setErrorMessage("-1 continuous seats is invalid");
         assertEquals(expectedResult, strategy.reserve(areaStatus, req));
 
-        requestedSeats.clear();
-        requestedSeats.add(new Seat(0, -1));
-        req.setSeats(requestedSeats);
-        expectedResult.setErrorMessage("event#A (0, -1) is not a valid seat.");
-        assertEquals(expectedResult, strategy.reserve(areaStatus, req));
-
-        requestedSeats.clear();
-        requestedSeats.add(new Seat(-1, 0));
-        req.setSeats(requestedSeats);
-        expectedResult.setErrorMessage("event#A (-1, 0) is not a valid seat.");
-        assertEquals(expectedResult, strategy.reserve(areaStatus, req));
-
-        requestedSeats.clear();
-        requestedSeats.add(new Seat(2, 0));
-        req.setSeats(requestedSeats);
-        expectedResult.setErrorMessage("event#A (2, 0) is not a valid seat.");
+        req.setNumOfSeats(0);
+        expectedResult.setErrorMessage("0 continuous seats is invalid");
         assertEquals(expectedResult, strategy.reserve(areaStatus, req));
     }
 }
