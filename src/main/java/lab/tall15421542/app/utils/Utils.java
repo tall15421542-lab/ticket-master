e39441e5a730834893859eb14e3459704e18b5b9
tall15421542-lab <tall15421542@gmail.com>
2025-03-12 09:35:26 +0800

    add properties file for event

--- src/main/java/lab/tall15421542/app/utils/Utils.java
+++ src/main/java/lab/tall15421542/app/utils/Utils.java
@@ -1,4 +1,25 @@
 package lab.tall15421542.app.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class Utils {
+    public static Properties readConfig(final String configFile) throws IOException {
+        // reads the client configuration from client.properties
+        // and returns it as a Properties object
+        if (!Files.exists(Paths.get(configFile))) {
+            throw new IOException(configFile + " not found.");
+        }
+
+        final Properties config = new Properties();
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            config.load(inputStream);
+        }
+
+        return config;
+    }
 }
