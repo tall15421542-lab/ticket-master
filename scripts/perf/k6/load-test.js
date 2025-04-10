35579bff50f96c973e893fe01aee383f3ecebfe2
tall15421542-lab <tall15421542@gmail.com>
2025-04-10 17:50:09 +0800

    add num of areas variable

--- scripts/perf/k6/load-test.js
+++ scripts/perf/k6/load-test.js
@@ -4,19 +4,53 @@ import { uuidv4, randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.4.0/ind
 import { Trend, Counter } from 'k6/metrics';
 import exec from 'k6/execution';
 
+export const options = {
+    discardResponseBodies: true,
+    scenarios: {
+        stress: {
+            executor: 'ramping-arrival-rate',
+            startRate: 1000,
+
+            timeUnit: '1s',
+
+            preAllocatedVUs: 1000,
+
+            stages: [
+                { target: 1000, duration: '5s' },
+
+                { target: 1500, duration: '5s' },
+
+                { target: 2000, duration: '1m' },
+
+                { target: 1000, duration: '5s' },
+            ],
+
+            gracefulStop: '10s',
+        },
+    },
+};
+
 const hostPort = __ENV.HOST_PORT ? `${__ENV.HOST_PORT}` : "localhost:8080"
 const baseURL = `http://${hostPort}`
+const numOfAreas = parseInt(__ENV.NUM_OF_AREAS ? `${__ENV.NUM_OF_AREAS}` : "1")
 
 export function setup(){
     const eventId = "event-" + uuidv4();
-    const payload = JSON.stringify({
+    const event = {
         eventName: `${eventId}`,
         artist: "k6 test",
-        areas: [
-            {areaId: "A", price: 1800, rowCount: 20, colCount: 30},
-            {areaId: "B", price: 1000, rowCount: 20, colCount: 30}
-        ]
-    });
+        areas: []
+    };
+
+    for(let areaIdx = 0 ; areaIdx < numOfAreas ; ++areaIdx){
+        event.areas.push({
+            areaId: areaIdx.toString(),
+            rowCount: 20,
+            colCount: 20,
+        })
+    }
+    
+    const payload = JSON.stringify(event)
 
     const params = {
         headers: {
@@ -34,44 +68,20 @@ export function setup(){
         throw new Error(`Setup failed: Expected status 200 but got ${res.status}`);
     }
 
-    return eventId
+    return event
 }
-
-export const options = {
-  discardResponseBodies: true,
-  scenarios: {
-    stress: {
-      executor: 'ramping-arrival-rate',
-      startRate: 1000,
-
-      timeUnit: '1s',
-
-      preAllocatedVUs: 1000,
-
-      stages: [
-        { target: 1000, duration: '5s' },
-
-        { target: 1500, duration: '5s' },
-
-        { target: 2000, duration: '1m' },
-
-        { target: 1000, duration: '5s' },
-      ],
-
-      gracefulStop: '10s',
-    },
-  },
-};
 
 const reservationTime = new Trend('reservation_time', true);
 const reservationCounter = new Counter('reservation_completed')
 
-export default function (eventId) {
+export default function (event) {
+    const eventId = event.eventName
     group('reserve seats', function(){
+        const areaIdx = Math.floor(Math.random() * numOfAreas);
         const payload = JSON.stringify({
             userId: "tall154215",
             eventId: `${eventId}`,
-            areaId: "A",
+            areaId: event.areas[areaIdx].areaId,
             numOfSeats: randomIntBetween(1, 4),
             type: "RANDOM"
         })
@@ -92,7 +102,7 @@ export default function (eventId) {
         if(reserve_seats_res.status != 200) {
             return;
         }
-        
+
         const reservationId = reserve_seats_res.body
         const get_reservation_url = `${baseURL}/v1/reservation/${reservationId}`
         const getParams = {
@@ -105,8 +115,8 @@ export default function (eventId) {
             'status is 200': (r) => r.status === 200,
         })
 
-        reservationTime.add(reserve_seats_res.timings.duration + get_reservation_res.timings.duration, [`${exec.vu.tags}`])
         if(get_reservation_res.status === 200){
+            reservationTime.add(reserve_seats_res.timings.duration + get_reservation_res.timings.duration, [`${exec.vu.tags}`])
             reservationCounter.add(1)
         }
     })
