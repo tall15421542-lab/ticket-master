c0fd0a0414f0f02a3d5c8deb0ee852f19f685ab4
tall15421542-lab <tall15421542@gmail.com>
2025-04-24 19:08:36 +0800

    create event for per iteration

--- scripts/perf/k6/smoke-test.js
+++ scripts/perf/k6/smoke-test.js
@@ -15,11 +15,7 @@ const hostPort = __ENV.HOST_PORT ? `${__ENV.HOST_PORT}` : "localhost:8080"
 const baseURL = `http://${hostPort}`
 const numOfAreas = parseInt(__ENV.NUM_OF_AREAS ? `${__ENV.NUM_OF_AREAS}` : "20")
 
-export function setup(){
+export default function () {
   const event = createEvent(baseURL, numOfAreas)
-  return event
-}
-
-export default function (event) {
   reserveSeats(baseURL, event, reservationTime, reservationCounter)
 }
